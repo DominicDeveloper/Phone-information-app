86ec4d8c5d44f11193fc46db20c55d69d1566fbc
DominicDeveloper <dominicazimov@gmail.com>
2024-08-13 21:02:58 +0500

    info

--- app/src/main/java/com/asadbek/phoneinforatmationsapp/MainActivity.kt
+++ app/src/main/java/com/asadbek/phoneinforatmationsapp/MainActivity.kt
@@ -3,10 +3,7 @@ package com.asadbek.phoneinforatmationsapp
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.asadbek.phoneinforatmationsapp.databinding.ActivityMainBinding
 
 /**
